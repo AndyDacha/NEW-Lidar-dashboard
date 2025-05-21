3bbeb8a2f53d3ff898757635479ee5b97722f139
Andrew Forsyth <andy@dacha-uk.com>
2025-05-21 20:50:45 +0100

    Describe your changes here

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -92,16 +92,6 @@ export default function LoginPage() {
             Sign In
           </button>
         </form>
-
-        <video
-          src="/Dacha SSI Ringwood Office Walkthrough COMPRESSED.mp4"
-          controls
-          loop
-          autoPlay
-          muted
-          className="rounded shadow border w-full"
-          style={{ height: '400px', maxHeight: '600px' }}
-        />
       </div>
     </div>
   );
